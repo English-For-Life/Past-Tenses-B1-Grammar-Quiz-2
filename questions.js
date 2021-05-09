f45eb0306ab02a1c81ff993f9d7c7c45f4f11951
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-05-09 20:52:03 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,78 @@
+//Array of objects
+const quiz = [
+    {
+        q:"When Patricia _____ into her car, she remembered she _____ the water boiler on.",
+        options:["got / had left", "was getting / has left", "had got / leaves", "got / was leaving"],
+        answer: 0
+    },
+    {
+     q:"We _____ our meal on the train as it _____ across the beautiful countryside.",
+        options:["had been enjoying / has gone", "were enjoying/ was going", "enjoyed / had gone", "have enjoyed / is going"],
+        answer: 1   
+    },
+    {
+        q:"They _____ for only a few minutes when they _____ a flat tyre.",
+        options:["have travelled / were having", "travelled / have had", "had been travelling / had", "were travelling / had"],
+        answer: 2
+    },
+    {
+        q:"Grandpa _____ his last car for thirty years when he _____ it last year.",
+        options:["was having / sold", "had / had sold", "had / was selling", "had had / sold"],
+        answer: 3
+    },
+    {
+        q:"Although he _____ his early childhood in Germany, he doesn't remember anything from that time now.",
+        options:["spent", "was spending", "had been spending", "has spent"],
+        answer: 0
+    },
+    {
+        q:"Vegetables and fruits don't taste as delicious as they _____ when I _____ a kid.",
+        options:["were doing / had been", "used to do / was", "have done / was", "did / have been"],
+        answer: 1
+    },
+    {
+        q:"What _____ the two _____ about when they started to quarrel ?",
+        options:["have / talked", "had been / talk", "were / talking", " had / talking"],
+        answer: 2
+    },
+    {
+        q:"Frank _____ to this city five years ago, but it seems he still _____ used to living with the heavy traffic here.",
+        options:["moved / didn't", "was moving / wasn't", "has moved / doesn't", "moved / isn't"],
+        answer: 3
+    },
+    {
+        q:"I _____ my pen, and I don't remember where I last _____ it.",
+        options:["have lost/used", "had lost / was using", "lost / had used", "have lost / have used"],
+        answer: 0
+    },
+    {
+        q:"It was a real surprise because she _____ no idea that they _____ a birthday party for her.",
+        options:["was having / organized", "had / had organized", "had / have been organizing", "has had / were organizing"],
+        answer: 1
+    },
+    {
+        q:"The baby _____ the living room floor with colour pens while her mother _____ the kitchen.",
+        options:["marked / has been mopping", "has marked / mopped", "was marking / was mopping", "had marked / had mopped"],
+        answer: 2
+    },
+    {
+     q:"There _____ some ketchup around his mouth because he _____ spaghetti.",
+        options:["has been / eats", "had been / has eaten", "is / had been eating", "was / had been eating"],
+        answer: 3   
+    },
+    {
+        q:"Because I _____ to eating with chopsticks, I _____ a hard time during the dinner.",
+        options:["wasn't used / had", "am not used / had been having", "hadn't been used / am having", "didn't use / was having"],
+        answer: 0
+    },
+    {
+        q:"When the film finished, I _____ the alarm clock and _____ to bed.",
+        options:["had sate / have gone", "set / went", "was setting / was going", "had set / is going"],
+        answer: 1
+    },
+    {
+        q:"Janette _____ all her friends for advice before she _____ to quit her job.",
+        options:["was asking / has decided", "has asked / decides", "had asked / decided", "has been asking / had decided"],
+        answer: 2
+    }
+]
